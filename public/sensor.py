efeb2f3e8dc7cb1b706b69c6af02fecc2bd9f591
i2x <0x8084@gmail.com>
2015-08-30 23:29:17 +0700

    auto path

--- public/sensor.py
+++ public/sensor.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+import os
 import random
 import time 
 
 var = 1
 while var == 1 :
-  f = open("/var/www/html/node/public/test.json", 'w')
-  x =random.uniform(26.9,27.3);
+
+  f = open(os.path.dirname(os.path.abspath(__file__))+"/test.json", 'w')
+  x =random.uniform(27,27.5);
   y =random.uniform(60.1,60.9);
   f.write('{\"temp\":'+str(x)+', \"hum\":'+str(y)+'}') 
-  time.sleep(1)  # Delay for 1 minute (60 seconds)
+  time.sleep(3)  # Delay for 1 minute (60 seconds)
 
